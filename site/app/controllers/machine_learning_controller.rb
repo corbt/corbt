ad1fa0f5d11e3443beace67465aae654fba6d5d1
Kyle Corbitt <kyle@corbt.com>
2013-11-07 08:03:36 -0700

    caching to disk instead of redis

--- site/app/controllers/machine_learning_controller.rb
+++ site/app/controllers/machine_learning_controller.rb
@@ -1,8 +1,5 @@
 class MachineLearningController < ApplicationController
 	def aggregate_data
-		# @data = Incident.current.where(region: params[:region])
-		# render json: @data.map{|incident| incident.hash}
-		cached_json = $redis.get("ml/regions/#{params[:region]}")
-		render inline: cached_json
+		render inline: "caching failed"
 	end
 end
