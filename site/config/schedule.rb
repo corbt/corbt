e8e2b82decf26eb5a0bdf479f678b81a09b3dcba
Kyle Corbitt <kyle@corbt.com>
2013-12-06 15:19:24 -0700

    turn off scraping

--- site/config/schedule.rb
+++ site/config/schedule.rb
@@ -3,18 +3,18 @@ env :PATH, ENV['PATH']
 
 set :output, "/data/apps/site/shared/log/cron_log.log"
 
-every 1.hours do
-	rake 'cs478:slurp_bing'
-end
+# every 1.hours do
+# 	rake 'cs478:slurp_bing'
+# end
 
-every 10.minutes do
-	rake 'cs598r:slurp_bing'
-end
+# every 10.minutes do
+# 	rake 'cs598r:slurp_bing'
+# end
 
-every 1.hours do
-	rake 'cs598r:cache_json'
-end
+# every 1.hours do
+# 	rake 'cs598r:cache_json'
+# end
 
-every 30.minutes do
-	rake 'cs598r:slurp_wu'
-end
+# every 30.minutes do
+# 	rake 'cs598r:slurp_wu'
+# end
