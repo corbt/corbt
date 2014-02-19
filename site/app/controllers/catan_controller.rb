dc1b35a66cb420967189ee79310b362cced087b8
Kyle Corbitt <kyle@corbt.com>
2014-02-18 18:10:52 -0700

    another try

--- site/app/controllers/catan_controller.rb
+++ site/app/controllers/catan_controller.rb
@@ -3,7 +3,7 @@ class CatanController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update
-    `/home/deploy/catan/update_server.sh`
+    system("/home/deploy/catan/update_server.sh")
 
     render nothing: true
   end
