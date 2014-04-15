54972be98d909869747eb062b0f7636bef317f78
Kyle Corbitt <kyle@corbt.com>
2014-04-14 22:58:45 -0600

    smarter i18n

--- site/app/controllers/application_controller.rb
+++ site/app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
+  private
+
+    def set_locale
+      I18n.locale = params[:locale] || env.http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+    end
 end
